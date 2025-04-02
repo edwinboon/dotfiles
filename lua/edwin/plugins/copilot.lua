10e7490f417f870c9500859e9479bb5c1e2318e1
Edwin Boon <hello@edwinboon.dev>
2025-04-02 09:21:03 +0200

    Fixed wrong config in my copilot setup

--- lua/edwin/plugins/copilot.lua
+++ lua/edwin/plugins/copilot.lua
@@ -1,12 +1,8 @@
 return {
 	"github/copilot.vim",
-	{
-		lazy = true,
-		event = "InsertEnter",
-		config = function()
-			vim.g.copilot_no_tab_map = true
-			vim.keymap.set("i", "<C-a>", 'copilot#Accept("<CR>")', { expr = true, silent = true, script = true })
-			vim.keymap.set("i", "<C-d>", "copilot#Dismiss()", { expr = true, silent = true, script = true })
-		end,
-	},
+	config = function()
+		vim.g.copilot_no_tab_map = true
+		vim.keymap.set("i", "<C-a>", 'copilot#Accept("<CR>")', { expr = true, silent = true, script = true })
+		vim.keymap.set("i", "<C-d>", "copilot#Dismiss()", { expr = true, silent = true, script = true })
+	end,
 }
