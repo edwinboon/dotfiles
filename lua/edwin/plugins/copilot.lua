42ad1418308422111a5d45fc0104f44659b69711
Edwin Boon <hello@edwinboon.dev>
2025-04-02 09:29:59 +0200

    Revert copilot changes for now

--- lua/edwin/plugins/copilot.lua
+++ lua/edwin/plugins/copilot.lua
@@ -1,8 +1,3 @@
 return {
 	"github/copilot.vim",
-	config = function()
-		vim.g.copilot_no_tab_map = true
-		vim.keymap.set("i", "<C-a>", 'copilot#Accept("<CR>")', { expr = true, silent = true, script = true })
-		vim.keymap.set("i", "<C-d>", "copilot#Dismiss()", { expr = true, silent = true, script = true })
-	end,
 }
