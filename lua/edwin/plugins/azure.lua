ffb091f27853fdfd9728e6a9b9b2b8a473a5f0fb
Edwin Boon <hello@edwinboon.dev>
2025-04-22 14:56:28 +0200

    Fixed issues with azure plugin

--- lua/edwin/plugins/azure.lua
+++ lua/edwin/plugins/azure.lua
@@ -2,18 +2,13 @@ return {
 	"edwinboon/azure.nvim",
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
-		local azfetch = require("azfetch")
+		local azure = require("azure")
 
-		azfetch.setup({
+		azure.setup({
 			decrypt = true,
 			keymaps = {
 				fetch_app_settings = "<leader>af",
 			},
 		})
-
-		-- Optionally, add a command or keymap for checking the version
-		vim.keymap.set("n", "<leader>azv", function()
-			azfetch.get_version()
-		end, { desc = "Check azfetch.nvim version" })
 	end,
 }
