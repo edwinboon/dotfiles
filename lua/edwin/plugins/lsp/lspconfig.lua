fc1f28975d4980a1a41f9eebb7d0b163b6128a8b
Edwin Boon <hello@edwinboon.dev>
2025-10-09 08:03:02 +0200

    update

--- lua/edwin/plugins/lsp/lspconfig.lua
+++ lua/edwin/plugins/lsp/lspconfig.lua
@@ -4,32 +4,38 @@ return {
 	dependencies = {
 		"hrsh7th/cmp-nvim-lsp",
 		{ "antosha417/nvim-lsp-file-operations", config = true },
-		{ "folke/neodev.nvim", opts = {} },
+		{ "folke/neodev.nvim", opts = {} }, -- neodev must load BEFORE configuring lua_ls
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
 	},
 	config = function()
-		-- Import required plugins
-		local lspconfig = require("lspconfig")
 		local mason = require("mason")
 		local mason_lspconfig = require("mason-lspconfig")
 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-		-- Setup mason
+		-- Ensure neodev is initialized (dependency has opts = {} so it's already called,
+		-- but calling safely again is harmless if you prefer explicitness)
+		pcall(function()
+			-- require("neodev").setup({})
+		end)
+
 		mason.setup()
 
-		-- Setup mason-lspconfig
-		mason_lspconfig.setup({
-			ensure_installed = { "lua_ls", "ts_ls", "graphql", "emmet_ls" },
-		})
+		-- Capabilities (reuse for every server)
+		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		-- Keymap utility for conciseness
-		local keymap = vim.keymap
+		-- Diagnostic signs
+		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		end
 
-		-- Define LSP keybindings
+		-- Keymaps on LSP attach
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 			callback = function(ev)
+				local keymap = vim.keymap
 				local opts = { buffer = ev.buf, silent = true }
 
 				opts.desc = "Show LSP references"
@@ -47,25 +53,25 @@ return {
 				opts.desc = "Show LSP type definitions"
 				keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
 
-				opts.desc = "See available code actions"
+				opts.desc = "Code actions"
 				keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
 
-				opts.desc = "Smart rename"
+				opts.desc = "Rename symbol"
 				keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 
-				opts.desc = "Show buffer diagnostics"
+				opts.desc = "Buffer diagnostics"
 				keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
 
-				opts.desc = "Show line diagnostics"
+				opts.desc = "Line diagnostics"
 				keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
 
-				opts.desc = "Go to previous diagnostic"
+				opts.desc = "Prev diagnostic"
 				keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
 
-				opts.desc = "Go to next diagnostic"
+				opts.desc = "Next diagnostic"
 				keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 
-				opts.desc = "Show documentation for what is under cursor"
+				opts.desc = "Hover documentation"
 				keymap.set("n", "K", vim.lsp.buf.hover, opts)
 
 				opts.desc = "Restart LSP"
@@ -73,67 +79,75 @@ return {
 			end,
 		})
 
-		-- Enable autocompletion capabilities
-		local capabilities = cmp_nvim_lsp.default_capabilities()
+		-- Servers we want installed (and then configured)
+		local servers = { "lua_ls", "ts_ls", "graphql", "emmet_ls" }
 
-		-- Customize diagnostic symbols
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
+		mason_lspconfig.setup({
+			ensure_installed = servers,
+			-- Using handlers lets Mason invoke configuration only after installation sanity
+			handlers = {
+				-- Default handler (for any server not specifically overridden below)
+				function(server_name)
+					-- Provide a default config only if we don't override below
+					if
+						server_name ~= "lua_ls"
+						and server_name ~= "graphql"
+						and server_name ~= "emmet_ls"
+						and server_name ~= "ts_ls"
+					then
+						local entry = vim.lsp.config[server_name]
+						if entry then
+							entry({
+								capabilities = capabilities,
+							})
+						end
+					end
+				end,
 
-		-- Configure LSP servers
-		local servers = { "ts_ls", "graphql", "emmet_ls", "lua_ls" }
-		for _, server_name in ipairs(servers) do
-			if server_name == "lua_ls" then
-				-- Special configuration for lua_ls
-				lspconfig.lua_ls.setup({
-					capabilities = capabilities,
-					settings = {
-						Lua = {
-							diagnostics = {
-								globals = { "vim" },
-							},
-							completion = {
-								callSnippet = "Replace",
+				["lua_ls"] = function()
+					vim.lsp.config.lua_ls({
+						capabilities = capabilities,
+						settings = {
+							Lua = {
+								diagnostics = { globals = { "vim" } },
+								completion = { callSnippet = "Replace" },
 							},
 						},
-					},
-				})
-			elseif server_name == "graphql" then
-				-- Special configuration for graphql
-				lspconfig.graphql.setup({
-					capabilities = capabilities,
-					filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-				})
-			elseif server_name == "emmet_ls" then
-				-- Special configuration for emmet_ls
-				lspconfig.emmet_ls.setup({
-					capabilities = capabilities,
-					filetypes = {
-						"html",
-						"typescript",
-						"javascript",
-						"typescriptreact",
-						"javascriptreact",
-						"css",
-						"scss",
-						"svelte",
-						"react",
-						"python",
-						"go",
-					},
-				})
-			elseif server_name == "ts_ls" then
-				-- Special configuration for ts_ls (TypeScript)
-				lspconfig.ts_ls.setup({})
-			else
-				-- Default server configuration
-				lspconfig[server_name].setup({
-					capabilities = capabilities,
-				})
-			end
-		end
+					})
+				end,
+
+				["graphql"] = function()
+					vim.lsp.config.graphql({
+						capabilities = capabilities,
+						filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+					})
+				end,
+
+				["emmet_ls"] = function()
+					vim.lsp.config.emmet_ls({
+						capabilities = capabilities,
+						filetypes = {
+							"html",
+							"typescript",
+							"javascript",
+							"typescriptreact",
+							"javascriptreact",
+							"css",
+							"scss",
+							"svelte",
+							"python",
+							"go",
+						},
+					})
+				end,
+
+				["ts_ls"] = function()
+					vim.lsp.config.ts_ls({
+						capabilities = capabilities,
+						-- Add ts_ls-specific settings/init_options if desired
+					})
+				end,
+			},
+		})
 	end,
 }
