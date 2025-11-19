c078336a27ec19254f15bf8f16969685b56339e5
Edwin Boon <hello@edwinboon.dev>
2025-11-19 14:16:21 +0100

    setup even more keymaps and add some docs for it

--- lua/edwin/plugins/snacks.lua
+++ lua/edwin/plugins/snacks.lua
@@ -7,16 +7,26 @@ return {
         keys = {
             -- Lazy git
             { "<leader>lg", function() require("snacks").lazygit() end, desc = "lazygit" },
-            { "<leader>gl", function() require("snacks").lazygit.log() end, desc = "lazygit logs" },
+            { "<leader>lgl", function() require("snacks").lazygit.log() end, desc = "lazygit logs" },
+            { "<leader>lgb", function() require("snacks").picker.git_branches({ layout = "select" }) end, desc = "pick and switch git branches" },
 
             -- File explorer
-            { "<leader>ef", function() require("snacks").explorer() end, desc = "open snack file explorer"}
+            { "<leader>ef", function() require("snacks").explorer() end, desc = "open snack file explorer" },
 
             -- File rename
-            { "<leader>rN" function() require("snacks").rename.rename_file() end, desc = "fast rename current file" },
+            { "<leader>rN", function() require("snacks").rename.rename_file() end, desc = "fast rename current file" },
 
             -- Buffer
             { "<leader>dB", function() require("snacks").bufdelete() end, desc = "delete or close buffer  (confirm)" },
+
+            -- Snacks Picker
+            { "<leader>ff", function() require("snacks").picker.files() end, desc = "find files" },
+            { "<leader>fc", function() require("snacks").picker.files({ cwd = "~/dotfiles/nvim/.config/nvim/lua" }) end, desc = "find nvim config files" },
+            { "<leader>fs", function() require("snacks").picker.grep() end, desc = "grep word" },
+            { "<leader>fws", function() require("snacks").picker.grep_word() end, desc = "search visual selection or word", mode = { "n", "x" } },
+            { "<leader>fk", function() require("snacks").picker.keymaps({ layout = "ivy" }) end, desc = "search keymaps (snacks picker)" },
+
+            { "<leader>vh", function() require("snacks").picker.help() end, desc = "help pages"}
         },
     }
 }
