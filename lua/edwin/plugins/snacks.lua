3b94bb83cfb6b9b5f99ba917876ae54b79aaf4b8
Edwin Boon <hello@edwinboon.dev>
2025-11-19 14:05:01 +0100

    add keymaps in snacks for oa lazygit

--- lua/edwin/plugins/snacks.lua
+++ lua/edwin/plugins/snacks.lua
@@ -1,11 +1,22 @@
 return {
     {
         "folke/snacks.nvim",
-        priority: 1000,
+        priority = 1000,
         lazy = false,
         opts = {},
         keys = {
+            -- Lazy git
+            { "<leader>lg", function() require("snacks").lazygit() end, desc = "lazygit" },
+            { "<leader>gl", function() require("snacks").lazygit.log() end, desc = "lazygit logs" },
 
-        }
+            -- File explorer
+            { "<leader>ef", function() require("snacks").explorer() end, desc = "open snack file explorer"}
+
+            -- File rename
+            { "<leader>rN" function() require("snacks").rename.rename_file() end, desc = "fast rename current file" },
+
+            -- Buffer
+            { "<leader>dB", function() require("snacks").bufdelete() end, desc = "delete or close buffer  (confirm)" },
+        },
     }
 }
