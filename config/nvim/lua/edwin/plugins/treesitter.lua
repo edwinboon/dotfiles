8c535f5c1c6c3699ffaeb2ade0e490635d127134
Edwin Boon <hello@edwinboon.dev>
2026-04-30 10:12:55 +0200

    add treesitter textobjects

--- config/nvim/lua/edwin/plugins/treesitter.lua
+++ config/nvim/lua/edwin/plugins/treesitter.lua
@@ -1,77 +1,93 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        event = { "BufReadPre", "BufNewFile" },
-        build = ":TSUpdate",
-        config = function()
-            local treesitter = require("nvim-treesitter.configs")
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = { "BufReadPre", "BufNewFile" },
+		build = ":TSUpdate",
 
-            -- Config treesitter
-            treesitter.setup({
-                -- enable highlighting
-                highlight = {
-                    enable = true,
-                },
-                -- enable indentation
-                indent = {
-                    enable = true,
-                },
-                -- ensure these languages parsers are installed
-                ensure_installed = {
-                    "json",
-                    "javascript",
-                    "typescript",
-                    "tsx",
-                    "go",
-                    "yaml",
-                    "html",
-                    "css",
-                    "python",
-                    "http",
-                    "markdown",
-                    "markdown_inline",
-                    "svelte",
-                    "graphql",
-                    "bash",
-                    "lua",
-                    "vim",
-                    "dockerfile",
-                    "gitignore",
-                },
-                incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        init_selection = "<C-space>",
-                        node_incremental = "<C-space>",
-                        scope_incremental = false,
-                    },
-                },
-                additional_vim_regex_highlighting = false,
-            })
-        end,
-    },
-    -- NOTE: js,ts,jsx,tsx Auto Close Tags
-    {
-        "windwp/nvim-ts-autotag",
-        enabled = true,
-        ft = { "html", "xml", "javascript", "typescript", "javascriptreact", "typescriptreact", "svelte" },
-        config = function()
-            -- Independent nvim-ts-autotag setup
-            require("nvim-ts-autotag").setup({
-                opts = {
-                    enable_close = true,           -- Auto-close tags
-                    enable_rename = true,          -- Auto-rename pairs
-                    enable_close_on_slash = false, -- Disable auto-close on trailing `</`
-                },
-                per_filetype = {
-                    ["html"] = {
-                        enable_close = true, -- Disable auto-closing for HTML
-                    },
-                    ["typescriptreact"] = {
-                        enable_close = true, -- Explicitly enable auto-closing (optional, defaults to `true`)
-                    },
-                },
-            })
-        end,
-    },
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
+
+		config = function()
+			local treesitter = require("nvim-treesitter.configs")
+
+			-- Config treesitter
+			treesitter.setup({
+				-- enable highlighting
+				highlight = {
+					enable = true,
+				},
+				-- enable indentation
+				indent = {
+					enable = true,
+				},
+				-- ensure these languages parsers are installed
+				ensure_installed = {
+					"json",
+					"javascript",
+					"typescript",
+					"tsx",
+					"go",
+					"yaml",
+					"html",
+					"css",
+					"python",
+					"http",
+					"markdown",
+					"markdown_inline",
+					"svelte",
+					"graphql",
+					"bash",
+					"lua",
+					"vim",
+					"dockerfile",
+					"gitignore",
+				},
+
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+						},
+					},
+				},
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<C-space>",
+						node_incremental = "<C-space>",
+						scope_incremental = false,
+					},
+				},
+				additional_vim_regex_highlighting = false,
+			})
+		end,
+	},
+	-- NOTE: js,ts,jsx,tsx Auto Close Tags
+	{
+		"windwp/nvim-ts-autotag",
+		enabled = true,
+		ft = { "html", "xml", "javascript", "typescript", "javascriptreact", "typescriptreact", "svelte" },
+		config = function()
+			-- Independent nvim-ts-autotag setup
+			require("nvim-ts-autotag").setup({
+				opts = {
+					enable_close = true, -- Auto-close tags
+					enable_rename = true, -- Auto-rename pairs
+					enable_close_on_slash = false, -- Disable auto-close on trailing `</`
+				},
+				per_filetype = {
+					["html"] = {
+						enable_close = true, -- Disable auto-closing for HTML
+					},
+					["typescriptreact"] = {
+						enable_close = true, -- Explicitly enable auto-closing (optional, defaults to `true`)
+					},
+				},
+			})
+		end,
+	},
 }
